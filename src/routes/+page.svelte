9fe2716f0d6057adbb6b3e4ef7546c25a89e7bb5
datnguyennnx <official.nguyendat@gmail.com>
2024-11-08 16:22:33 +0700

    initial codebase

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { Alert } from 'flowbite-svelte'
+	import Sidebar from '$lib/components/layout/Sidebar/Sidebar.svelte'
+</script>
+
+<div class="flex flex-grow">
+	<Sidebar />
+</div>
