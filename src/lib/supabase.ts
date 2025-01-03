8042cbfc2a000c1e18ea91c7f5da619860a3f830
datnguyennnx <official.nguyendat@gmail.com>
2025-01-03 19:28:16 +0700

    fix: import env variable following sveltekit

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,10 +1,8 @@
 // src/lib/supabase.ts
 import { createClient } from '@supabase/supabase-js'
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
 	auth: {
 		persistSession: true, // Enable session persistence
 		autoRefreshToken: true, // Enable token auto-refresh
